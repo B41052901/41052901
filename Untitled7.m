e68fb8339767ce2791f380b2fa1f948ee5bbff46
B41052901 <63893439+B41052901@users.noreply.github.com>
2020-04-18 12:35:55 +0800

    Add files via upload

---
+++ Untitled7.m
@@ -0,0 +1,9 @@
+%學號:B41052901
+%姓名:陳信彰
+clear
+A=[123];
+B=[-1 3 -1];
+% dot
+c=dot(A,B)
+%cross
+D=cross(A,B)
---
+++ spower.m
@@ -0,0 +1,4 @@
+%學號:B41052901
+%姓名:陳信彰
+function p=spower(x)
+p=(norm(x)^2)/length(x);
